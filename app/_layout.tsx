7ba03c89caa96b56b4e6404287f909253dac2c1f
MahalakshmiPFW <maha.01@pfw.edu>
2025-11-25 15:32:42 -0500

    change the name of the screen from "index" to "Pokemon Explorer"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="index" options={{ title: "Pokemon Explorer" }} />
+    <Stack.Screen name="pok_details" options={{ title: "Pokemon Details" }} />
+  </Stack>;
 }
